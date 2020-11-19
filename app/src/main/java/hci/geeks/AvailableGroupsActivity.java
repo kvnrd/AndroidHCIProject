91f7b88e89c846b611612b5901c4eb877a7f9166
Mauricio Hidalgo <mrhidalgo@miners.utep.edu>
2020-11-19 02:35:57 -0700

    Waiting for Kevin's code

--- app/src/main/java/hci/geeks/AvailableGroupsActivity.java
+++ app/src/main/java/hci/geeks/AvailableGroupsActivity.java
@@ -3,6 +3,7 @@ package hci.geeks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,9 @@ public class AvailableGroupsActivity extends AppCompatActivity {
     GroupsDatabaseHelper db;
     ListView groupsList;
     GroupsListAdapter adapter;
+    TextView groupName;
+    int groupId;
+    String name;
     //Group group;
 
     @Override
@@ -26,11 +30,18 @@ public class AvailableGroupsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle("Geeks");
         setContentView(R.layout.activity_available_groups);
+        groupName = findViewById(R.id.groupName);
         db = new GroupsDatabaseHelper(this);
         groups.setGroups(db.allGroups());
         if(groups.getGroups() == null){
             groups.newList();
         }
+
+       // Intent intent = getIntent();
+
+      // groupId = intent.getIntExtra("Id", 0);
+       // name = intent.getStringExtra("Name");
+
         groupsList = findViewById(R.id.groupsListView);
         registerForContextMenu(groupsList);
         adapter = new GroupsListAdapter(this, 0, groups.getGroups());
@@ -62,4 +73,10 @@ public class AvailableGroupsActivity extends AppCompatActivity {
             return row;
         }
     }
+
+    public void createGroupButtonClicked(View view){
+       // Intent intent = new Intent(this, AppointmentsActivity.class); //Kevin's class
+       // startActivity(intent);
+    }
+
 }
