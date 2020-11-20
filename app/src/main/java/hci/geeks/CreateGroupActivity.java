f3894adf89f53b83528c0dbccc16eb2a5039ba76
Mauricio Hidalgo <mrhidalgo@miners.utep.edu>
2020-11-20 15:59:44 -0700

    Kevin's Class included

---
+++ app/src/main/java/hci/geeks/CreateGroupActivity.java
@@ -0,0 +1,23 @@
+package hci.geeks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+
+    public void createClicked(View view){
+
+    }
+
+    public void cancelClicked(View view){
+
+    }
+}
